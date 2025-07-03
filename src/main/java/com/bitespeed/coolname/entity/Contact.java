e7b6e55558e8d26606a065811d8d91bb16332844
Shah Mohd. Arhum <mohd.arhum28@gmail.com>
2025-07-03 16:45:28 +0530

    Add Entity

---
+++ src/main/java/com/bitespeed/coolname/entity/Contact.java
@@ -0,0 +1,41 @@
+package com.bitespeed.coolname.entity;
+
+import com.bitespeed.coolname.entity.base.Base;
+import com.bitespeed.coolname.model.enums.LinkPrecedence;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "contact")
+public class Contact extends Base {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "contact_id", unique = true, nullable = false, updatable = false)
+    private String contactId;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "linked_id")
+    private String linkedId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "link_precedence", nullable = false)
+    private LinkPrecedence linkPrecedence;
+
+    @PrePersist
+    public void onInsert(){
+        this.contactId = UUID.randomUUID().toString();
+    }
+}
---
+++ src/main/java/com/bitespeed/coolname/entity/base/Base.java
@@ -0,0 +1,36 @@
+package com.bitespeed.coolname.entity.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class Base {
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void onInsert(){
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void onUpdate(){
+        this.updatedAt = LocalDateTime.now();
+    }
+}
+
